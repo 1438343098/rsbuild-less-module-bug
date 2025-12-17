a523804ad6bced100fee25b794d31725f86463c2
liuhanliang <liuhanliang@kuaizi.ai>
2025-12-17 17:32:12 +0800

    完善模版

--- rsbuild.config.js
+++ rsbuild.config.js
@@ -1,48 +1,189 @@
 import { defineConfig } from '@rsbuild/core'
 import { pluginVue2 } from '@rsbuild/plugin-vue2'
-// 注意：移除了手动引入的 VueLoaderPlugin
-import { pluginLess } from '@rsbuild/plugin-less' // 建议改用 import 语法
+import { pluginBabel } from '@rsbuild/plugin-babel'
+import { pluginVue2Jsx } from '@rsbuild/plugin-vue2-jsx'
+import { pluginNodePolyfill } from '@rsbuild/plugin-node-polyfill'
+import { pluginBasicSsl } from '@rsbuild/plugin-basic-ssl'
+import path from 'path'
+const resolve = dir => path.resolve(__dirname, dir)
 
 export default defineConfig({
   plugins: [
-    // 配置 pluginVue2 插件以支持 style module
-    pluginVue2({
-      vueLoaderOptions: {
-        // 关键配置：启用 CSS Modules 并设置类名格式
-        cssModules: {
-          localIdentName: '[local]--[hash:base64:5]' // 使用了更简洁的类名格式
-        }
+    pluginNodePolyfill(),
+    pluginBabel({
+      // 处理子包
+      include: [/src/, /@kuaizi\/saas-components/],
+      babelLoaderOptions: {
+        presets: [['@babel/preset-env', { targets: 'defaults' }]]
       }
     }),
-    pluginLess()
+    pluginVue2(),
+    // jsx
+    pluginVue2Jsx(),
+    // ssl
+    pluginBasicSsl()
   ],
   source: {
     entry: {
       index: './src/main.js'
+    },
+    // 对齐process.env
+    define: {
+      ...(function () {
+        return Object.fromEntries(
+          Object.entries(process.env).map(([key, value]) => [
+            `process.env.${key}`,
+            JSON.stringify(value)
+          ])
+        )
+      })()
+    }
+  },
+  // @符号
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      // 兼容vue cdn注入和
+      vue$: require.resolve('vue/dist/vue.runtime.esm.js')
+      // vuex$: require.resolve('vuex')
     }
   },
   html: {
     template: './public/index.html',
+    // 注入环境变量
     templateParameters: (compilation, assets, assetTags) => {
       return {
-        title: 'test',
-        assetPrefix: assets.publicPath
+        // 兼容 Vue CLI 的 htmlWebpackPlugin 语法 之前webpack 的html直接写这里就不需要修改html了
+        htmlWebpackPlugin: {
+          options: {
+            title: 'test',
+            assetPrefix: assets.publicPath
+          }
+        },
+        autoprefixer: {
+          browsers: ['> 1%', 'last 2 versions', 'not dead']
+        }
       }
     }
   },
-  // 确保 CSS Modules 的自动识别开启
   output: {
     cssModules: {
-      auto: resourcePath => {
-        // 对 .module.css/.module.less 文件或 Vue 文件内的 scoped/style module 启用 CSS Modules
-        return (
-          /\.module\.\w+$/.test(resourcePath) || resourcePath.includes('.vue')
-        )
-      }
+      auto: /\.module\.(css|less|scss|sass)$/,
+      localIdentName: '[local]--[hash:base64:5]'
     }
   },
-  // 重要：Vue 2 项目需要关闭 experiments.css
+  // Vue 2 项目需要关闭 experiments.css
   experiments: {
     css: false
+  },
+  // 关闭性能提示 一些日志可以点开查看
+  performance: {
+    hints: false
+  },
+  server: {
+    // https: true 如果需要https
+  },
+  tools: {
+    rspack(config, { addRules }) {
+      // externals 兼容cdn注入
+      // config.externals = {
+      //   vue: 'Vue',
+      //   vuex: 'Vuex',
+      //   'vue-router': 'VueRouter',
+      //   'vue-i18n': 'VueI18n',
+      //   'element-ui': 'ELEMENT',
+      //   axios: 'axios'
+      // }
+
+      config.module.rules = (config.module.rules || []).filter(rule => {
+        return !(rule.test && rule.test.toString().includes('less'))
+      })
+
+      // 全局css变量注入
+      const lessVars = {
+        colorPrimary: '#0066ff',
+        colorPrimaryLight8: '#d4e7ff',
+        colorSuccess: '#3ec07d',
+        colorWarning: '#f97c56',
+        colorDanger: '#f56c6c',
+        colorBg: 'white',
+        colorBorder: '#999',
+        colorText: '#777b7e',
+        colorTitle: '#1d2328',
+        colorDark: 'rgba(0, 0, 0, 0.75)',
+        headerHeight: '64px',
+        minWidth: '1200px',
+        sideBarWidth: '210px',
+        font: '14px',
+        fontSmall: '12px',
+        bg: 'white'
+      }
+      // svg + rules 和less处理
+      addRules([
+        {
+          test: /\.less$/,
+          oneOf: [
+            {
+              // 处理 <style module lang="less">
+              resourceQuery: /module/,
+              use: [
+                'vue-style-loader',
+                {
+                  loader: 'css-loader',
+                  options: {
+                    modules: {
+                      localIdentName: '[local]--[hash:base64:5]'
+                    }
+                  }
+                },
+                'postcss-loader',
+                {
+                  loader: 'less-loader',
+                  options: {
+                    lessOptions: {
+                      javascriptEnabled: true,
+                      globalVars: lessVars
+                    }
+                    // additionalData: sharedLessImports
+                  }
+                }
+              ]
+            },
+            {
+              // 普通 less
+              use: [
+                'vue-style-loader',
+                'css-loader',
+                // 需要有postcss.config.js
+                'postcss-loader',
+                {
+                  loader: 'less-loader',
+                  options: {
+                    lessOptions: {
+                      javascriptEnabled: true,
+                      globalVars: lessVars
+                    }
+                    // 注入所有style 顶部的 css代码例如 @import "${resolve('src/style/theme.less')}";
+                    // additionalData: sharedLessImports
+                  }
+                }
+              ]
+            }
+          ]
+        },
+        {
+          test: /\.svg$/,
+          include: [resolve('src/asset/icons/svg')],
+          use: [
+            {
+              loader: require.resolve('svg-sprite-loader'),
+              options: {
+                symbolId: 'icon-[name]'
+              }
+            }
+          ]
+        }
+      ])
+    }
   }
 })
--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <div :class="moduleStyle.text">123123</div>
+    <div :class="moduleStyle['text']">123123</div>
     <div class="text2">123123</div>
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
@@ -27,8 +27,18 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+.user-info-popover {
+  border: none;
+  border-radius: 16px;
+  padding: 16px;
+  background: #ffffff;
+  box-shadow: 0px 12px 40px 0px rgba(1, 18, 58, 0.15);
+}
 .text {
   color: red;
+  &div {
+    color: blue;
+  }
 }
 </style>
 <style lang="less" scoped>
